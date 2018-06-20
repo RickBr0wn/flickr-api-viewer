5c52194eb507f09a058054486cce3ea7ec736bc8
RiCK BROWN <ricky.brown.00@gmail.com>
2018-06-20 19:51:30 +0100

    bugged!

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,14 +1,27 @@
 import React from 'react'
 
 import './searchStyles.css'
+import keys from '../../config/keys'
 
 class Search extends React.Component {
-  //     apiURL: 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=ca3783111609d69139840916b7a01ad2&format=json&nojsoncallback=1&per_page=5'
+  constructor(){
+    super()
+    this.state = {
+      textInput: ''
+    }
+  }
 
   onChange = event => {
-    this.setState({ [event.target.name]: event.target.value}, () => {
-      
-    })
+    const testURL = `https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=${keys.flickrKey}&format=json&nojsoncallback=1&per_page=5`
+    this.setState({ [event.target.name]: event.target.value}, 
+      () => {
+        console.log('FETCH!!')
+        // fetch(testURL)
+        //   .then(res => res.json())
+        //   .then(data => console.log(data))
+        //   .catch(error => console.log(error))
+      }
+    )
   }
 
   render() {
@@ -17,9 +30,9 @@ class Search extends React.Component {
         <div className="empty-space-large"></div>
         <form className="form__group" autoComplete="off">
           <div className="icon-holder">
-            <i class="material-icons">search</i>
+            <i className="material-icons">search</i>
           </div>
-          <input type="text" className="form__input" placeholder="Search for image" id="search" />
+          <input type="text" className="form__input" placeholder="Search for image" id="search" name={this.state.textInput} value={this.state.textInput} onChange={this.onChange} />
           <label htmlFor="search" className="form__label">Search for image</label>
         </form>
       </div>
